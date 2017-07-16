cd1192281ab6b8e31bce98c4f1e9503a362ded96
Nikolay Vashchenko <sir.nickolas@gmail.com>
2017-07-16 04:56:51 +0300

    extension code

---
+++ ext/stopgap_13632/extconf.rb
@@ -0,0 +1,13 @@
+require 'mkmf'
+
+extension_name = 'stopgap_13632'
+
+dir_config(extension_name)
+
+BROKEN_VERSIONS = %w(2.2.7 2.3.4 2.4.1)
+if (BROKEN_VERSIONS.include? RUBY_VERSION)
+  $defs << '-DBROKEN_RUBY'
+  $defs << "-DVERSION_#{RUBY_VERSION.gsub('.', '_')}"
+end
+
+create_makefile(extension_name )
